7d69f21a0ccf37eb5f70df595a81c4b5d229ef62
Emidio Croci <emidio.croci@gmail.com>
2013-03-12 19:58:47 +0100

    Project creation

---
+++ ArtPrize/Controllers/VoteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtPrize.Controllers
+{
+    public class VoteController : Controller
+    {
+        //
+        // GET: /Vote/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Vota() 
+        {
+            return View();
+        }             
+    }
+}
